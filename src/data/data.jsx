0d63494d964f6754fc27ebc192d8c929bd24e515
Alan Ruiz <matiasruizc315@gmail.com>
2024-09-09 21:00:15 -0400

    assets update

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -7,7 +7,7 @@ export default [
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://res.cloudinary.com/jnto/image/upload/w_750,h_503,fl_lossy,f_auto/v1531981666/fujiguide/SG010_6"
+        imageUrl: "./src/assets/monte-fuji.jpg"
     }
 ,
     {
@@ -18,7 +18,7 @@ export default [
         startDate: "27 May, 2021",
         endDate: "8 Jun, 2021",
         description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. It is one of the 20th century's most famous and distinctive buildings.",
-        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a0/Sydney_Australia._%2821339175489%29.jpg"
+        imageUrl: "./src/assets/opera.jpg"
     },
     {
         id: 3,
@@ -28,6 +28,6 @@ export default [
         startDate: "01 Oct, 2021",
         endDate: "18 Nov, 2021",
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-        imageUrl: "https://cdn.sanity.io/images/4aans0in/production/07cf4fd838ea156369b369da37ec4d8e69b7aa42-2000x1390.jpg?fm=webp&fit=max&auto=format"
+        imageUrl: "./src/assets/geira.jpg"
     }
 ]
